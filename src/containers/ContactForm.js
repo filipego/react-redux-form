880424bbdcdb001a0db1a0fc4228f29fd425840a
Filipe Valente Gomes <Filipe@Filipes-iMac.fios-router.home>
2017-09-14 17:53:35 -0400

    added placeholder

--- src/containers/ContactForm.js
+++ src/containers/ContactForm.js
@@ -29,11 +29,11 @@ const submit = ({ firstName = "", lastName = "", email = "" }) => {
   }
 }
 
-const renderField = ({ label, input, type, meta: { touched, error } }) => (
+const renderField = ({ label, input, placeholder, type, meta: { touched, error } }) => (
   <div className="input-row">
     <label>{label}</label>
     <br />
-    <input {...input} type={type}/>
+    <input {...input} placeholder={placeholder} type={type}/>
     {touched && error &&
      <span className="error">{error}</span>}
   </div>
@@ -44,7 +44,7 @@ let ContactForm = ({ handleSubmit }) => {
     <form onSubmit={ handleSubmit(submit) }>
         <Field name="firstName" label="First Name" component={renderField} type="text" />
         <Field name="lastName" label="First Name" component={renderField} type="text" />
-        <Field name="email" label="Email" component={renderField} type="email" />
+        <Field name="email" label="Email" placeholder="email@email.com" component={renderField} type="email" />
         <button type="submit">Submit</button>
     </form>
   )
